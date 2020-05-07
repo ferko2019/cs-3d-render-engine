a0d46e97ec189324a005c9788506c5cde29e4d8c
RockGam <jferike2007@gmail.com>
2020-05-07 14:54:51 +0200

    ngbjt

--- RenderEngine/3DObject.cs
+++ RenderEngine/3DObject.cs
@@ -8,7 +8,12 @@ namespace _3D_Render_Engine_Whit_UI.RenderEngine
 {
     public class _3DObject
     {
-        public Face[] faces;
-        public Vector[] verticies;
+        public Face[] faces = new Face[0];
+        public Vector[] verticies = new Vector[0];
+        public string name = "unknown";
+        public string path = "C:\\";
+        public Vector pos = new Vector() { x = 0, y = 0, z = 0 };
+        public Vector rotation = new Vector() { x = 0, y = 0, z = 0 };
+        public Vector scale = new Vector() { x = 1, y = 1, z = 1 };
     }
 }
--- RenderEngine/ObjDecoder.cs
+++ RenderEngine/ObjDecoder.cs
@@ -13,10 +13,27 @@ namespace _3D_Render_Engine_Whit_UI.RenderEngine
         public static _3DObject LoadObj(string path, EngineUi form)
         {
             _3DObject obj = new _3DObject();
+            obj.pos = new Vector() { x = 0, y = 0, z = 0 };
+            obj.rotation = new Vector() { x = 0, y = 0, z = 0 };
+            obj.scale = new Vector() { x = 1, y = 1, z = 1 };
             string[] raw_file = File.ReadAllLines(path);
+            List<Vector> vertices = new List<Vector>();
             foreach (string row in raw_file)
             {
-                form.ConsolePrint(row);
+                string[] row_splitted = row.Split(' ');
+                if (row_splitted[0].Contains("o"))
+                {
+                    obj.name = row_splitted[2];
+                }else if (row_splitted[0].Contains("v"))
+                {
+
+                }else if (row_splitted[0].Contains("vn"))
+                {
+
+                }else if (row_splitted[0].Contains("f"))
+                {
+
+                }
             }
             return obj;
         }
